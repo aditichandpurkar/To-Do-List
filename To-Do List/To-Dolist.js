f5ed4ea02d84951a4e20171c3064d36e0dbe4260
Aditi Chandpurkar <149958227+aditichandpurkar@users.noreply.github.com>
2024-06-22 00:26:10 +0530

    Add files via upload

---
+++ To-Do List/To-Dolist.js
@@ -0,0 +1,19 @@
+
+
+function addTask(){
+	const newTask = document.createElement('li')
+	const tasklist = document.getElementById('taskList')
+	taskList.appendChild(newTask)
+	newTask.textContent = document.getElementById('taskInput').value //whatever we are writing in the inputbox it will be visible
+	document.getElementById('taskInput').value=" " //the input box will be empty after we added the value
+	delTask(newTask)
+
+}
+function delTask(newTask){
+	const deleteButton = document.createElement('button')
+	deleteButton.textContent = 'Delete'
+	newTask.appendChild(deleteButton)
+	deleteButton.onclick = function(){
+		newTask.remove()
+	}
+}
