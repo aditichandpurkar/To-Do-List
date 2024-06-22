d8bf6e4e2dac6d1f40a29571c55ec50857567041
Aditi Chandpurkar <149958227+aditichandpurkar@users.noreply.github.com>
2024-06-22 10:07:08 +0530

    To-Dolist.js

--- To-Do List/To-Dolist.js
+++ To-Do List/To-Dolist.js
@@ -5,7 +5,7 @@ function addTask(){
 	const tasklist = document.getElementById('taskList')
 	taskList.appendChild(newTask)
 	newTask.textContent = document.getElementById('taskInput').value //whatever we are writing in the inputbox it will be visible
-	document.getElementById('taskInput').value=" " //the input box will be empty after we added the value
+	document.getElementById('taskInput').value="" //the input box will be empty after we added the value
 	delTask(newTask)
 
 }
